b9a5f7c9e30c6bdd66ebe7cb68ccc3bd09b9b4de
Alexandru Gagea <alexandrugagea@gmail.com>
2025-05-26 15:50:34 +0300

    added more tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,3 +6,21 @@ def test_education_success(client):
 def test_education_unauthorized(client):
     res = client.get("/education")
     assert res.status_code == 401
+
+def test_experience_success(client):
+    res = client.get("/experience?token=supersecrettoken123")
+    assert res.status_code == 200
+    assert isinstance(res.get_json(), list)
+
+def test_experience_unauthorized(client):
+    res = client.get("/experience")
+    assert res.status_code == 401
+
+def test_personal_success(client):
+    res = client.get("/personal?token=supersecrettoken123")
+    assert res.status_code == 200
+    assert isinstance(res.get_json(), dict)
+
+def test_personal_unauthorized(client):
+    res = client.get("/personal")
+    assert res.status_code == 401
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,20 @@
 from click.testing import CliRunner
 from app.cli import print_cv
 
-def test_cli_output():
+def test_cli_output_personal():
     runner = CliRunner()
     result = runner.invoke(print_cv)
     assert result.exit_code == 0
     assert "Personal" in result.output
+
+def test_cli_output_experience():
+    runner = CliRunner()
+    result = runner.invoke(print_cv)
+    assert result.exit_code == 0
+    assert "Experience" in result.output
+
+def test_cli_output_education():
+    runner = CliRunner()
+    result = runner.invoke(print_cv)
+    assert result.exit_code == 0
+    assert "Education" in result.output
